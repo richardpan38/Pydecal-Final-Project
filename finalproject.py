c495137a6d7788b0131880718f805a53d3a56ae6
rpanman <rpanman@berkeley.edu>
2017-04-13 19:45:10 -0700

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -1,4 +1,9 @@
 #Python Project
+#Average of redshit
+#plug into Da formula
+#Plug into Ev equation
+#GUI Python
+
 
 class Galaxy(object):
 	def __init__(self, redshift, avglamb, deltalamb, extent):
@@ -6,5 +11,9 @@ class Galaxy(object):
 		self.avglamb = avglamb
 		self.deltalamb = deltalamb
 		self.extent = extent
-Hello 
+		self.expectedvel = #expected velocity given mass of galaxy 
+			   	   #error in observed mass and mass online
+				   #error in observation redshift and online redshit
+				   #create a plot of graph
+				   #Table of calculated versus online (ranking) 
 
