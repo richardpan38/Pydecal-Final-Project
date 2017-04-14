22098bf6f0bf1a85f4fd398d0aee7812d05b3280
justinsidhu <justinsidhu@berkeley.edu>
2017-04-13 19:05:02 -0700

    test

---
+++ finalproject.py
@@ -0,0 +1,9 @@
+#Python Project
+
+class Galaxy(object):
+	def __init__(self, redshift, avglamb, deltalamb, extent):
+		self.redshift = redshift
+		self.avglamb = avglamb
+		self.deltalamb = deltalamb
+		self.extent = extent
+
