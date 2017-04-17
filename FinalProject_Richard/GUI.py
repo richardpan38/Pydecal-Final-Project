af71b41f8dcf1273f8115f5684794c903197e954
Richard Pan <RichardP@airbears2-10-142-31-194.airbears2.1918.berkeley.edu>
2017-04-16 17:41:32 -0700

    Added a few things

--- FinalProject_Richard/GUI.py
+++ FinalProject_Richard/GUI.py
@@ -1,19 +1,30 @@
+#2 frames.
+#frame 1 will have space to insert numbers on the left side. Set these numbers to a variable that will be part of the program later on.
+#Buttons in frame 1 to run the program and print the results on the right side.
+#Redshift, average wavelength, change in wavelength, vertical extent as blanks
+#Right side print values
+#Another button to graph onto frame 2
+#Frame 2 will show a graph of the values
 import tkinter as tk
+import matplotlib 
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_TkAgg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure 
 
 LARGE_FONT = ("Verdana", 12)
 
-class RotationCurves(Game):
+class RotationCurves(Frame):
 	def __init__(self):
-		Game.__init(self,master)
-		container = tk.Game(self)
+		Frame.__init(self,master)
+		container = tk.Frame(self)
 		container.pack(side = 'top', fill = 'both', expand = True)
 		container.grid_rowconfigure(0, weight = 1)
 		container.grid_columnconfigure(0, weight = 1)
-		self.Game = ()
+		self.Frame = ()
 		for F in (StartPage, PageOne): 
 
 			frame = F(container, self)
-			self.Game[F] = frame
+			self.Frame[F] = frame
 			frame.grid(row = 0, column = 0, sticky = "nsew")
 		self.show_frame(StartPage)
 
@@ -21,6 +32,7 @@ class RotationCurves(Game):
 	def show_frame(self, cont):
 		frame = self.Game[cont]
 		frame.tkraise()
+
 class StartPage(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
@@ -29,11 +41,15 @@ class StartPage(tk.Frame):
 		button1 = tk.Button(self, text = 'Solve') #This button runs the rotation curve solver
 		button1.grid() #Where the button is on the GUI
 		button2 = tk.Button(self, text = 'Graph', command = lambda: controller.show_frame(PageOne)) #Graphs the curve on a different grame. Second command brings the frame forward.
-class StartPage(tk.Frame):
+class PageOne(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 		laebl =tk.Label(self, text = 'Graph' , font = LARGE_FONT)
-		label.grid()
+		label.pack(pady=10, padx=10)
+		f = Figure(figsize = (5,5), dpi = 100)
+		canvas = FigureCanvasTkAgg(f, self)
+		canvas.show()
+		canvas .get_tk_widget().pack(side = tk.TOP, fill = tk.BOTH, expand = True)
 
 
 app = RotationCurves()
