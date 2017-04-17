1531eaaef61d7360163e28ebc4f5dab3a455d78f
Justin Sidhu <justin@airbears2-10-142-62-138.airbears2.1918.berkeley.edu>
2017-04-16 17:56:45 -0700

    should be working

--- FinalProject_Justin/numcrunch.py
+++ FinalProject_Justin/numcrunch.py
@@ -11,25 +11,20 @@ def get_angdist(rshift):
     Dm = 1130.2
     Da = (Dm/(1+rshift))
     Da = Da * (3.0*10**(22))
-    return Da
-    print('Da = ' + Da)
+    print('Angular distance diameter = ', Da, ' meters.')
 
 def conv_vertext(vertext):
     vertext = (vertext * 0.1185 * np.pi) / (3600 * 180)
-    return vertext
+    print('Converted vertical extent = ', vertext, ' radians.')
 
 def get_radi(Da,vertext):
     radi = (Da * vertext) / 2
-    return radi
-    print(radi)
+    print('Galactic radius = ', radi, ' meters.')
 
 def get_vel(delt_wl,avg_wl):
     vel = (delt_wl * np.c) / (avg_wl * 2)
-    return vel
+    print('Galactic rotation speed = ', vel, ' meters per second.')
 
 def get_mass(radi,vel):
     mass = (radi * (vel**2)) / np.G
-    return mass
-
-
-get_angdist(0.5)
+    print('Galactic mass = ', mass, ' kilograms.')
