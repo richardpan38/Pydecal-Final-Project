af8623399aca2d3426e935334defbb29fd91eb13
justinsidhu <justin@jaspreet.org>
2017-04-18 15:50:49 -0700

    why is it asking me to add this

--- FinalProject_Justin/toolbar.py
+++ FinalProject_Justin/toolbar.py
@@ -4,24 +4,25 @@ from tkinter import *
 class Example(Frame):
 
     def __init__(self,*args, **kwargs):
-        Frame.__init__(self, *args, **kwargs)   
-         
-        self.parent = parent        
+        Frame.__init__(self, *args, **kwargs)
+
+        parent = None
+        self.parent = parent
         self.UI()
-        
-        
+
+
     def UI(self):
-      
+
         self.parent.title("Menu")
-        
+
         menubar = Menu(self.parent)
         self.parent.config(menu=menubar)
-        
+
         fileMenu = Menu(menubar)
         fileMenu.add_command(label="Exit", command=self.onExit)
         fileMenu.add_command(label="Save", command=self.filewrite)
         menubar.add_cascade(label="File", menu=fileMenu)
-        
+
         editMenu = Menu(menubar)
         menubar.add_cascade(label="Edit", menu=editMenu)
         editMenu.add_command(label="Universe", command=self.onExit)
@@ -41,7 +42,7 @@ class Example(Frame):
         #file = open("data.txt")
         #file.write(textFile2)
         #textObj.insert(INSERT, file.read())
-        #file.close()    
+        #file.close()
 
 app = Example()
 app.mainloop()
